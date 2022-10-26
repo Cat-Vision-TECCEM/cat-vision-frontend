9773cfd0f9879fa8e3d4466680e1819b58fd9ecd
Diego Ulibarri <diegoulhe@outlook.com>
2022-10-25 21:17:07 -0500

    Display products availability

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,34 +1,56 @@
 import styles from "../styles/Map.module.css";
-import { useState, useMemo, useCallback, useRef } from "react";
-import { 
+import { useState, useMemo, useEffect, useCallback, useRef } from "react";
+import {
   useLoadScript,
   GoogleMap,
   Marker,
   DirectionsRenderer,
   Circle,
-  MarkerClusterer, 
+  MarkerClusterer,
 } from "@react-google-maps/api";
 
+type LatLngLiteral = google.maps.LatLngLiteral;
+type MapOptions = google.maps.MapOptions;
 
-function Map(){
-  const center = useMemo(() => ({lat: 19.43, lng: -99.13}), []) 
+function Map() {
+  const [stores, setStores] = useState([]);
+  const getStores = async () => {
+    const storesData = await fetch("http://127.0.0.1:8080/store/getAllStores");
+    const jsonStores = await storesData.json();
+    setStores(jsonStores.stores);
+  };
+
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: "AIzaSyCRrWXwt1eidGW-sK-5DdDU-nWYUoiLSkk",
-    libraries: ["places"]
+    libraries: ["places"],
   });
+  const center = useMemo<LatLngLiteral>(
+    () => ({ lat: 19.43, lng: -99.13 }),
+    []
+  );
+  const options = useMemo<MapOptions>(
+    () => ({
+      mapId: "d4c619bb7dbcd015",
+      disableDefaultUI: true,
+      clickableIcons: false,
+    }),
+    []
+  );
 
-  if (!isLoaded) return <div>Loading...</div>
+  useEffect(() => {
+    getStores();
+  }, []);
 
+  if (!isLoaded) return <div>Loading...</div>;
   return (
-    <GoogleMap 
+    <GoogleMap
       zoom={12}
       center={center}
       mapContainerClassName="card"
+      options={options}
       mapContainerStyle={{ width: "100%" }}
-    >
-
-    </GoogleMap>
-  )
+    ></GoogleMap>
+  );
 }
 
-export default Map
+export default Map;
--- pages/dashboard/stores.tsx
+++ pages/dashboard/stores.tsx
@@ -1,34 +1,70 @@
 import { NextPage } from "next";
 import Select from "react-select";
 import PageNavigation from "../../components/PageNavigation";
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useId, useState } from "react";
+import Image from "next/image";
 
 interface Store {
-  id: number,
-  address: string,
-  label: string,
-  value: string
+  id: number;
+  address: string;
+  label: string;
+  value: string;
 }
 
 const stores: NextPage = () => {
-  const [stores, setStores] = useState([]);
-
-  const getStores = async () => {
-    const storesData = await fetch("http://127.0.0.1:8080/store/getAllStores");
-    const jsonStores = await storesData.json();
-    setStores(jsonStores.stores)
-  };
-
   const st = {
     id: 1,
     address: "",
     label: "",
-    value: ""
+    value: "",
+  };
+
+  const today = new Date();
+  const [stores, setStores] = useState([]);
+  const [store, setStore] = useState<Store>(st);
+  const [startDate, setStartDate] = useState(`${today.getFullYear()}-${today.getMonth() + 1}`);
+  const [startMonth, setStartMonth] = useState(`${today.getMonth() + 1}`);
+  const [startYear, setStartYear] = useState(`${today.getFullYear()}`);
+  const [endMonth, setEndMonth] = useState(`${today.getMonth() + 2}`);
+  const [endYear, setEndYear] = useState(`${today.getFullYear()}`);
+
+  const [products, setProducts] = useState<any[]>([]);
+  const [sales, setSales] = useState<any[]>([]);
+  const [earnings, setEarnings] = useState<number>(0);
+  const [totalSaleProducts, setTotalSaleProducts] = useState<number>(0);
+
+  const getStores = async () => {
+    const storesData = await fetch("http://127.0.0.1:8080/store/getAllStores");
+    const jsonStores = await storesData.json();
+    setStores(jsonStores.stores);    
+  };
+
+  const handleStartDateInput = (e: ChangeEvent<HTMLInputElement>) => {
+    const selectedDate = e.target.value
+    setStartYear(selectedDate.slice(0, 4));
+    setStartMonth(selectedDate.slice(5));
+    setStartDate(selectedDate);
   }
 
-  const getData = async (store: Store) => {
-    console.log(store.id);
+  const handleEndDateInput = (e: ChangeEvent<HTMLInputElement>) => {
+    const selectedDate = e.target.value
+    setEndYear(selectedDate.slice(0, 4));
+    setEndMonth(selectedDate.slice(5));
   }
+
+  const getData = async () => {
+    const storeProducts = await fetch(
+      `http://127.0.0.1:8080/store/getProducts?store_id=${store.id}`
+    );
+    const jsonProducts = await storeProducts.json();
+    setProducts(jsonProducts.products);
+    const salesData = await fetch(
+      `http://127.0.0.1:8080/order/getSalesProduct?start_month=${startMonth}&start_year=${startYear}&store_id=${store.id}&end_month=${endMonth}&end_year=${endYear}`
+    );
+    const salesJSON = await salesData.json();
+    console.log(salesJSON);
+    
+  };
 
   useEffect(() => {
     getStores();
@@ -38,14 +74,57 @@ const stores: NextPage = () => {
     <div className="dashboardContainer">
       <PageNavigation />
       <div className="dashboardContentStore" style={{ paddingBottom: 0 }}>
-        <Select options={stores} isSearchable onChange={(store) => getData(store ? store : st)}/>
+        <div className="filters">
+          <Select
+            options={stores}
+            instanceId={useId()}
+            isSearchable
+            onChange={(store) => setStore(store ? store : st)}
+          />
+          <div className="dashboardInputs">
+            <label htmlFor="">Fecha Inicio: </label>
+            <input type="month" min="2022-08" value={startDate} onChange={(e) => handleStartDateInput(e)} required/>
+          </div>
+          <div className="dashboardInputs">
+            <label htmlFor="">Fecha Fin: </label>
+            <input type="month" min="2022-08" onChange={(e) => handleEndDateInput(e)}/>
+          </div>
+          <input type="submit" value="Filtrar" onClick={getData}/>
+        </div>
+        <div className="card cardTop"></div>
+        <div className="card cardTop"></div>
       </div>
 
       <div className="dashboardContentStore">
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
+        <div className="card twoSpaces">
+          {products.map( (product, index) => {
+            return(
+              <div className="productChart" key={index}>
+                <Image 
+                  loader={()=>product.url}
+                  src={product.url}
+                  alt="Imagen producto"
+                  width={100}
+                  height={100}
+                  unoptimized
+                />
+                <div className="productChartInfo">
+                  <p>{product.name}</p>
+                  <p>
+                    Estado: 
+                    {product.stock ? 
+                      <span style={{color: "green"}}> Existente</span>: 
+                      <span style={{color: "red"}}> Agotado</span>
+                    }
+                  </p>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+        <div className="card">
+          
+        </div>
         <div className="card"></div>
         <div className="card"></div>
         <div className="card"></div>
