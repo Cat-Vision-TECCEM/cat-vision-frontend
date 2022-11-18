17bce8fa9870c61a5e64e53f013dda72430b0f27
RamirezFernanda <ramirez.bmfernanda@gmail.com>
2022-11-18 14:46:29 -0600

    Update pages

--- pages/grocery_stores/providers.tsx
+++ pages/grocery_stores/providers.tsx
@@ -1,11 +1,44 @@
 import { NextPage } from "next";
-import React, { useState } from "react";
-import NewProviderCard from "../../components/NewProviderCard"
+import React, { useState, useId } from "react";
+import NewProviderCard from "../../components/NewProviderCard";
+import GroceriesNavigation from "../../components/GroceriesNavigation";
+import Select from "react-select";
+
+interface Providers {
+  id: number;
+  address: string;
+  label: string;
+  value: string;
+}
 
 const providers: NextPage = () => {
-    return(
-        <NewProviderCard/>
-    )
+  const providersNames = [
+    {
+      name: "Femsa"
+    }
+  ];
+  const pr = {
+  id: 1,
+  address: "",
+  label: "Buscar Proveedor",
+  };
+
+  const [providers, setProviders] = useState([])
+
+  return(
+    <div>
+      <GroceriesNavigation/>
+      <div className="search-bar">
+        <Select 
+        options={providers}
+        instanceId={useId()}
+        isSearchable
+        defaultValue={ pr }
+        />
+      </div>
+      <NewProviderCard/>
+    </div>
+  )
 }
 
 export default providers
